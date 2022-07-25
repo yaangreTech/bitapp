693f3721d583825491f6b4af89a06f0f6215de54
lougoudoro <slougoudoro@gmail.com>
2022-07-25 12:14:05 +0000

    update

---
+++ resources/views/errors/419.blade.php
@@ -0,0 +1,35 @@
+<x-other>
+    <x-slot name="other_head">
+        <div>
+            {{-- <a  onclick="location.href='/'" style="cursor: pointer;">
+               Logout?
+            </a> --}}
+        </div>
+    </x-slot>
+
+    <div class="login100-form">
+        <span class="error-header p-b-45">
+            419
+        </span>
+        <span class="error-subheader p-b-5">
+            POOR SPEED
+        </span>
+        <span class="error-subheader2 p-b-5">
+            Look like your sessions ar expired!!!
+        </span>
+        <div class="container-login100-form-btn p-t-30	">
+            <button class="login100-form-btn" onclick="location.href='javascript:history.back()'">
+                Go back</button>
+            </button>
+        </div>
+        <div class="w-full p-t-15 p-b-15 text-center">
+            <div>
+                {{-- <a href="#" class="txt1">
+                    Need Help?
+                </a> --}}
+            </div>
+        </div>
+    </div>
+
+
+</x-other>
