7c9f782891463d8446c52bb229627f17c912373b
lougoudoro <slougoudoro@gmail.com>
2022-08-13 14:56:41 +0000

    todqy

---
+++ database/seeders/GeneralSeeder.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class GeneralSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        $tables=['departements','branches', 'levels','semesters','tus','modules','years','promotions','students','parentes',
+            'inscriptions',
+            'tests',
+            'marks'
+    ];
+        try {
+            foreach ($tables as $table){
+                try {
+                    $datas = file_get_contents(base_path("database/data/".$table.'.json'));
+                    $datas=json_decode($datas, true);
+                    // dd($datas[$table]);
+                    foreach ($datas[$table] as $data){
+                      
+                        DB::table($table)->insert($data);
+                        print_r($data);
+                    }
+                } catch (\Throwable $th) {
+                    dd( $table);
+                }
+               
+               
+            }
+        } catch (\Throwable $th) {
+           dd('$th->getMessage()');
+        }
+            
+    }
+}
