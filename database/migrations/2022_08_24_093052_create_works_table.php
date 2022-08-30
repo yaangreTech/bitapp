ffb48e3515c19fe2a56099c1237e42cef203543b
lougoudoro <slougoudoro@gmail.com>
2022-08-30 10:16:32 +0000

    dsdfsfdf
    d

---
+++ database/migrations/2022_08_24_093052_create_works_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWorksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('works', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('alumni_id')->constrained()->onDelete('cascade')->onUpdate('cascade');
+            $table->foreignId('company_id')->constrained()->onDelete('cascade')->onUpdate('cascade');
+            $table->string('poste');
+            $table->date('start_date');
+            $table->date('end_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('works');
+    }
+}
